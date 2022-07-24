c0c84c3bbb03821e53a0b8a367616dba36b2a5b0
Sunken Ahn <devsanso@outlook.com>
2022-07-24 23:23:04 +0900

    dto 작성 #11

---
+++ src/main/java/com/github/devsanso/sns/dto/PostVODto.java
@@ -0,0 +1,34 @@
+package com.github.devsanso.sns.dto;
+
+
+import com.github.devsanso.sns.entity.PostEntity;
+import com.github.devsanso.sns.entity.UserEntity;
+import com.github.devsanso.sns.vo.PostVO;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PostVODto {
+    private PostVO vo;
+
+    private String DataUrl() {
+        if(vo.headerImageDataUrl.isEmpty())return null;
+
+        return vo.headerImageDataUrl.get();
+    }
+
+    public PostEntity toEntity(UserEntity user) {
+        return PostEntity.builder()
+                .user(user)
+                .title(vo.title)
+                .content(vo.content)
+                .ImageDataUrl(DataUrl())
+                .build();
+    }
+    public PostEntity toNoUserEntity() {
+        return PostEntity.builder()
+                .title(vo.title)
+                .content(vo.content)
+                .ImageDataUrl(DataUrl())
+                .build();
+    }
+}
---
+++ src/main/java/com/github/devsanso/sns/dto/UserEntityDto.java
@@ -0,0 +1,10 @@
+package com.github.devsanso.sns.dto;
+
+import com.github.devsanso.sns.entity.UserEntity;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UserEntityDto {
+    private UserEntity entity;
+
+}
