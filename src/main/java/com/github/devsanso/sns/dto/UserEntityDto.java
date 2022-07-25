2add8f6ab9a819320ea731590d0a9e95f94c0922
Sunken Ahn <devsanso@outlook.com>
2022-07-25 14:36:05 +0900

    유저 엔티티 dto 생성 - close #25

--- src/main/java/com/github/devsanso/sns/dto/UserEntityDto.java
+++ src/main/java/com/github/devsanso/sns/dto/UserEntityDto.java
@@ -1,10 +1,26 @@
 package com.github.devsanso.sns.dto;
 
 import com.github.devsanso.sns.entity.UserEntity;
+import com.github.devsanso.sns.vo.UserInfoVO;
 import lombok.AllArgsConstructor;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 public class UserEntityDto {
     private UserEntity entity;
 
+    private Optional<String> imageDataUrlOption(String image) {
+        if(image == null)return Optional.empty();
+
+        return Optional.of(image);
+    }
+    public UserInfoVO toUserInfoVO() {
+        var profile =entity.getUserProfile();
+        return UserInfoVO.builder()
+                .userUUID(entity.getUserUUID().toString())
+                .name(profile.name)
+                .imageDataUrl(imageDataUrlOption(profile.imageDataUrl))
+                .build();
+    }
 }
